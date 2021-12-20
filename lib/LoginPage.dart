4a2407b8f9ebe943d3d4f2a474198fd2e48884af
Alper Alp <alperalp2004@hotmail.com>
2021-12-20 20:05:07 +0300

    login page textFields

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,14 +1,20 @@
-import 'dart:html';
-import 'dart:ui';
-
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/sabitler.dart';
 
-class LoginPage extends StatelessWidget {
+bool passVisibility = false;
+
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
+
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.black,
@@ -32,26 +38,63 @@ class LoginPage extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      "Instagram",
-                      style: TextStyle(
-                          fontFamily: "InstagramBanner",
-                          color: Colors.white,
-                          fontSize: 50),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 25),
+                      child: Text(
+                        "Instagram",
+                        style: TextStyle(
+                            fontFamily: "InstagramBanner",
+                            color: Colors.white,
+                            fontSize: 50),
+                      ),
                     ),
                     Padding(
                       padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 20),
+                          horizontal: 20, vertical: 7),
                       child: TextField(
+                        cursorColor: Colors.white,
+                        style: TextStyle(color: Colors.white.withAlpha(190)),
                         decoration: InputDecoration(
                             labelText:
                                 "Telefon numarası,e-posta adresi veya kullanici adi",
-                            fillColor: Colors.white38.withAlpha(50),
+                            fillColor: Colors.white38.withAlpha(70),
+                            labelStyle: TextStyle(color: Colors.white70),
                             filled: true,
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(10))),
                       ),
-                    )
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 7),
+                      child: TextField(
+                        cursorColor: Colors.white,
+                        style: TextStyle(color: Colors.white.withAlpha(190)),
+                        decoration: InputDecoration(
+                            labelText:
+                            "Şifre",
+                            fillColor: Colors.white38.withAlpha(70),
+                            suffixIcon: IconButton(icon: Icon(passVisibility ? Icons.visibility:Icons.visibility_off,color: Colors.white70 ,),onPressed: (){
+                              setState(() {
+                                passVisibility ? passVisibility = false: passVisibility = true;
+                                log(passVisibility.toString());
+                              });
+
+                            },),
+                            labelStyle: TextStyle(color: Colors.white70),
+                            filled: true,
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                        obscureText: !passVisibility,
+                      ),
+                    ),
+
+
+
                   ],
                 ),
               )
