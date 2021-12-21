fe1dacfa41a3eb58be7d1784c5a8a1a2e3304462
Alper Alp <alperalp2004@hotmail.com>
2021-12-21 22:59:19 +0300

    login page button and divider no footer

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,5 +1,8 @@
 import 'dart:developer';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:instagram_clone/sabitler.dart';
 
 bool passVisibility = false;
@@ -45,37 +48,38 @@ class _LoginPageState extends State<LoginPage> {
                         style: TextStyle(
                             fontFamily: "InstagramBanner",
                             color: Colors.white,
-                            fontSize: 50),
+                            fontSize: 41),
                       ),
                     ),
                     Padding(
                       padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 7),
+                          horizontal: 30, vertical: 7),
                       child: TextField(
                         cursorColor: Colors.white,
-                        style: TextStyle(color: Colors.white.withAlpha(190)),
+                        style: TextStyle(color: Colors.white.withAlpha(190),fontSize: 12),
                         decoration: InputDecoration(
                             labelText:
-                                "Telefon numarası,e-posta adresi veya kullanici adi",
+                                "Telefon numarası,e-posta adresi veya kullanıcı adı",
                             fillColor: Colors.white38.withAlpha(70),
-                            labelStyle: TextStyle(color: Colors.white70),
+                            labelStyle: TextStyle(color: Colors.white70,fontSize: 14),
                             filled: true,
                             floatingLabelBehavior: FloatingLabelBehavior.never,
-                            focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                            focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
                             border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10))),
+                                borderRadius: BorderRadius.circular(5))),
                       ),
                     ),
                     Padding(
                       padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 7),
+                          horizontal: 30, vertical: 7),
                       child: TextField(
                         cursorColor: Colors.white,
-                        style: TextStyle(color: Colors.white.withAlpha(190)),
+                        style: TextStyle(color: Colors.white.withAlpha(190),fontSize: 12),
                         decoration: InputDecoration(
                             labelText:
                             "Şifre",
                             fillColor: Colors.white38.withAlpha(70),
+
                             suffixIcon: IconButton(icon: Icon(passVisibility ? Icons.visibility:Icons.visibility_off,color: Colors.white70 ,),onPressed: (){
                               setState(() {
                                 passVisibility ? passVisibility = false: passVisibility = true;
@@ -83,15 +87,58 @@ class _LoginPageState extends State<LoginPage> {
                               });
 
                             },),
-                            labelStyle: TextStyle(color: Colors.white70),
+                            labelStyle: TextStyle(color: Colors.white70,fontSize: 14),
                             filled: true,
                             floatingLabelBehavior: FloatingLabelBehavior.never,
-                            focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                            focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
                             border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10))),
+                                borderRadius: BorderRadius.circular(5))),
                         obscureText: !passVisibility,
                       ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 7),
+                      child: Row(
+                        children: [
+                          Expanded(child: Container(
+                            height: 49,
+                            child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 14),
+                            child: Text("Giriş Yap",style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.bold),),
+                          ),alignment: Alignment.center,decoration: BoxDecoration(borderRadius: BorderRadius.circular(5),color: Colors.blue.shade700),)),
+
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5,horizontal: 20),
+                      child: Row(children: [
+                        Text("Giriş detaylarını mı unuttun?",style: TextStyle(color: Colors.white38.withAlpha(170),fontSize: 12),),
+                        Text(" Giriş yapmak için yardım al" , style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 12))
+                      ],
+                        mainAxisAlignment: MainAxisAlignment.center,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 15,horizontal: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+
+                        Expanded(child: Divider(color: Colors.white70)),
+                        Text("   YA DA   ",style: TextStyle(color: Colors.white.withAlpha(190),fontWeight: FontWeight.bold),),
+                        Expanded(child: Divider(color:Colors.white70)),
+                      ],),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                      Icon(Icons.facebook,color: Colors.blue,size: 31,),
+                      Text("  Facebook ile Giriş Yap",style:TextStyle(color:Colors.blue,fontWeight: FontWeight.bold,fontSize: 16))
+                      ],
+                    )
+
+
 
 
 
