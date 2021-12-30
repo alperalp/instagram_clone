db93ec8997e622e6e13d72f64d2162e5ad4672cc
Alper Alp <alperalp2004@hotmail.com>
2021-12-30 19:33:17 +0300

    login page done

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,13 +1,25 @@
+// ignore_for_file: file_names, prefer_const_constructors
+
 import 'dart:developer';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:instagram_clone/sabitler.dart';
 
 bool passVisibility = false;
 TextEditingController emailController = TextEditingController();
 TextEditingController passwordController = TextEditingController();
+
+bool loginButtonAvaible = false;
+double loginButtonOpacity = 0.5;
+
+String defaultEmail = "test123@gmail.com";
+String defaultPassword = "test123";
+  
+late BuildContext buildContext;
+
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -72,6 +84,46 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
+  Padding emailTextField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+          horizontal: 30, vertical: 7),
+      child: Container(
+        height: 45,
+        child: TextField(
+          controller: emailController,
+          onChanged: (text){
+            setState(() {
+              if(text == "" || passwordController.text == ""){
+                loginButtonAvaible = false;
+
+
+              }
+              else{
+                loginButtonAvaible = true;
+              }
+
+              loginButtonAvaible ? loginButtonOpacity = 1.0 : loginButtonOpacity=0.5;
+
+            });
+
+          },
+          cursorColor: Colors.white,
+          style: TextStyle(color: Colors.white.withAlpha(190),fontSize: 16),
+          decoration: InputDecoration(
+              labelText:
+              "Telefon numarası,e-posta adresi veya kullanıcı adı",
+              fillColor: Colors.white38.withAlpha(70),
+              labelStyle: TextStyle(color: Colors.white70,fontSize: 14),
+              filled: true,
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+              focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5))),
+        ),
+      ),
+    );
+  }
 
   Padding passwordTextField() {
     return Padding(
@@ -81,16 +133,18 @@ class _LoginPageState extends State<LoginPage> {
                       height: 45,
                       child: TextField(
                         controller: passwordController,
+
                         onChanged: (text){
                           setState(() {
-                            if(text != "" && emailController.text != ""){
-                              loginButtonAvaible = true;
+                            if(text == "" || emailController.text == ""){
+                              loginButtonAvaible = false;
 
                             }
                             else{
-                              loginButtonAvaible = false;
+                              loginButtonAvaible = true;
                             }
-                            log(loginButtonAvaible.toString());
+                            loginButtonAvaible ? loginButtonOpacity = 1.0 : loginButtonOpacity=0.5;
+
                           });
 
                         },
@@ -145,56 +199,6 @@ class instagramBanner extends StatelessWidget {
 }
 
 
-class emailTextField extends StatefulWidget {
-  const emailTextField({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<emailTextField> createState() => _emailTextFieldState();
-}
-
-class _emailTextFieldState extends State<emailTextField> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-          horizontal: 30, vertical: 7),
-      child: Container(
-        height: 45,
-        child: TextField(
-          controller: emailController,
-          onChanged: (text){
-            setState(() {
-              if(text != "" && emailController.text != ""){
-                loginButtonAvaible = true;
-
-              }
-              else{
-                loginButtonAvaible = false;
-              }
-              log(loginButtonAvaible.toString());
-            });
-
-          },
-          cursorColor: Colors.white,
-          style: TextStyle(color: Colors.white.withAlpha(190),fontSize: 16),
-          decoration: InputDecoration(
-              labelText:
-                  "Telefon numarası,e-posta adresi veya kullanıcı adı",
-              fillColor: Colors.white38.withAlpha(70),
-              labelStyle: TextStyle(color: Colors.white70,fontSize: 14),
-              filled: true,
-              floatingLabelBehavior: FloatingLabelBehavior.never,
-              focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5))),
-        ),
-      ),
-    );
-  }
-}
-bool loginButtonAvaible = false;
 class loginButton extends StatefulWidget {
   const loginButton({
     Key? key,
@@ -207,22 +211,42 @@ class loginButton extends StatefulWidget {
 class _loginButtonState extends State<loginButton> {
   @override
   Widget build(BuildContext context) {
+    buildContext = context;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 7),
-      child: Row(
-        children: [
-          Expanded(child: Container(
-            height: 45,
-            child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 14),
-            child: Text("Giriş Yap",style: TextStyle(color: Colors.white.withOpacity(loginButtonAvaible? 1.0 : 0.5),fontSize: 15,fontWeight: FontWeight.bold),),
-          ),alignment: Alignment.center,decoration: BoxDecoration(borderRadius: BorderRadius.circular(5),color: Colors.blue.shade700.withOpacity(loginButtonAvaible? 1.0 : 0.5)),)),
+      child: GestureDetector(
+        onTapDown: (detail) => setState(() {
+          if(loginButtonAvaible){
+            loginButtonOpacity = 0.5;
+          }
+        }),
+        onTapUp: (detail) => setState(() {
+          if(emailController.text == defaultEmail && passwordController.text == defaultPassword){
+            //do something
+          }
+          else{
+            showDialog(context: buildContext, builder: (_) => wrongLoginDialog);
+          }
 
-        ],
+
+          loginButtonOpacity = 1.0;
+        }),
+        child: Row(
+          children: [
+            Expanded(child: Container(
+              height: 45,
+              child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 14),
+              child: Text("Giriş Yap",style: TextStyle(color: Colors.white.withOpacity(loginButtonOpacity),fontSize: 15,fontWeight: FontWeight.bold),),
+            ),alignment: Alignment.center,decoration: BoxDecoration(borderRadius: BorderRadius.circular(5),color: Colors.blue.shade700.withOpacity(loginButtonOpacity)),)),
+
+          ],
+        ),
       ),
     );
   }
 }
+
 
 class forgotPassword extends StatelessWidget {
   const forgotPassword({
@@ -280,3 +304,41 @@ class facebookLinkText extends StatelessWidget {
     );
   }
 }
+
+Dialog wrongLoginDialog = Dialog(
+  backgroundColor: Colors.transparent,
+  child: Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 10),
+    child: Container(
+      height:275,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Color.fromRGBO(39, 36, 37, 1) ),
+      child:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Hesap Bulunamıyor",style: TextStyle(color: Color.fromRGBO(245, 245, 245,1),fontSize: 24,fontWeight: FontWeight.bold ),),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 12),
+              child: Text(emailController.text+" ile bir hesap bulamıyoruz. Başka bir telefon numarası veya e-posta dene yoksa, kaydolabilirsin.",textAlign: TextAlign.center,style: TextStyle(color: Color.fromRGBO(133, 130, 131, 1.0)),),
+            ),
+            Divider(color: Colors.white60.withAlpha(150),),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10,),
+              child: GestureDetector(
+                onTap: (){Navigator.pop(buildContext);},
+                child: Text("Tekrar Dene",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18,color: Colors.blue,),)),
+            ),
+            Divider(color: Colors.white60.withAlpha(150),),
+            Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: Text("Kaydol",style: TextStyle(fontSize: 16,color: Colors.white,),),
+            ),
+            
+          ],
+        ),
+      ),
+    ),
+  ),
+
+
+);
